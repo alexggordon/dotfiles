633c7f57f37ae63a49a8fa351ef0eae753528bbd
Alex Gordon <alexggordon@MacBook-Pro.localdomain>
2024-10-28 15:28:23 -0400

    fixes

--- init/50_text_editors.sh
+++ init/50_text_editors.sh
@@ -3,7 +3,7 @@ mkdir -p "$HOME/Library/Application Support/Code/User/"
 ln -s "$HOME/.config/apps/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
 # sublime
-ln -s "$HOME/.config/apps/sublime/" "$HOME/Library/Application Support/Sublime Text/"
+ln -s "$HOME/.config/apps/sublime" "$HOME/Library/Application Support/Sublime Text"
 
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
